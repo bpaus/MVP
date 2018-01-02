0488e6487943daba843e41e4d8a5a17d72c223e7
Roni <ron1.cozx@gmail.com>
2018-01-02 17:37:20 +0200

    full therapy cycle

---
+++ bpausV1/new/src/firebaseConnection.js
@@ -0,0 +1,10 @@
+console.log("init firebase");
+var config = {
+    apiKey: "AIzaSyDh-xolf0-e1HOVjWAhu-XCob6phWAV6HQ",
+    authDomain: "bpaus-mvp.firebaseapp.com",
+    databaseURL: "https://bpaus-mvp.firebaseio.com",
+    projectId: "bpaus-mvp",
+    storageBucket: "",
+    messagingSenderId: "136326559135"
+};
+firebase.initializeApp(config);
